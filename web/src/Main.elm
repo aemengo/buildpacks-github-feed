32272275096525ea5787596152183117f2ad6749
Anthony Emengo <aemengo@vmware.com>
2021-05-28 23:00:32 -0400

    Touch up refresh note
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -10,7 +10,7 @@ import Json.Decode as D
 import Models exposing (..)
 import String.Extra exposing (ellipsis)
 import Svg exposing (path, svg)
-import Svg.Attributes exposing (d, fill, viewBox)
+import Svg.Attributes exposing (d, fill, fillRule, viewBox)
 import Time
 
 
@@ -257,6 +257,12 @@ viewRepo repo =
 view : Model -> Html msg
 view model =
     let
+        refresh =
+            svg [ Svg.Attributes.class "text-white-50 me-2 bi bi-arrow-repeat", width 16, height 16, viewBox "0 0 16 16", fill "currentColor" ]
+                [ path [ d "M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" ] []
+                , path [ d "M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z", fillRule "evenodd" ] []
+                ]
+
         content =
             case model of
                 Failure txt ->
@@ -276,7 +282,10 @@ view model =
                     [ img [ src "/assets/img/buildpacks-icon.png", alt "logo", width 30, height 25, class "d-inline-block align-text-top mx-4" ] []
                     , text "Activity"
                     ]
-                , small [ class "fw-light fst-italic text-white-50 me-5" ] [ text "updates every 5min" ]
+                , span []
+                    [ refresh
+                    , small [ class "fw-light fst-italic text-white-50 me-5" ] [ text "5m" ]
+                    ]
                 ]
             ]
         , div [ class "container" ] content
