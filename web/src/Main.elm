75ed2017db5d9c8e9e88fb2499aab346dadf22f9
Anthony Emengo <aemengo@vmware.com>
2021-09-03 23:39:56 -0400

    Add rotate animation when loading data
    Signed-off-by: Anthony Emengo <aemengo@vmware.com>

--- web/src/Main.elm
+++ web/src/Main.elm
@@ -274,8 +274,16 @@ viewRepo repo =
 view : Model -> Html msg
 view model =
     let
+        refreshClassList =
+            case model of
+                Loading ->
+                    "text-white-50 me-1 bi bi-arrow-repeat rotate"
+
+                _ ->
+                    "text-white-50 me-1 bi bi-arrow-repeat"
+
         refresh =
-            svg [ Svg.Attributes.class "text-white-50 me-1 bi bi-arrow-repeat", width 16, height 16, viewBox "0 0 16 16", fill "currentColor" ]
+            svg [ Svg.Attributes.class refreshClassList, width 16, height 16, viewBox "0 0 16 16", fill "currentColor" ]
                 [ path [ d "M11.534 7h3.932a.25.25 0 0 1 .192.41l-1.966 2.36a.25.25 0 0 1-.384 0l-1.966-2.36a.25.25 0 0 1 .192-.41zm-11 2h3.932a.25.25 0 0 0 .192-.41L2.692 6.23a.25.25 0 0 0-.384 0L.342 8.59A.25.25 0 0 0 .534 9z" ] []
                 , path [ d "M8 3c-1.552 0-2.94.707-3.857 1.818a.5.5 0 1 1-.771-.636A6.002 6.002 0 0 1 13.917 7H12.9A5.002 5.002 0 0 0 8 3zM3.1 9a5.002 5.002 0 0 0 8.757 2.182.5.5 0 1 1 .771.636A6.002 6.002 0 0 1 2.083 9H3.1z", fillRule "evenodd" ] []
                 ]
